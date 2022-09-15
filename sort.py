eca280bd6dc941ee9850adb492430bf70a4efc08
Iridum-png <63092175+Iridum-png@users.noreply.github.com>
2022-09-15 12:46:36 +0100

    Changed bubble sort optimisation

--- sort.py
+++ sort.py
@@ -6,8 +6,8 @@ def bubble_sort(arr: list) -> list :
     return arr
 
 def optimised_bubble_sort(arr: list) -> list :
-    for _ in range(len(arr)):
-        for j in range(len(arr) - 1):
+    for i in range(len(arr)):
+        for j in range(len(arr) - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
